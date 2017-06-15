dccf991b770de2022a6b4b0757f98ad8402bb80d
全玲玲 <aleen@quanlinglingdeMacBook-Pro.local>
2017-06-15 22:48:07 +0800

    Initial Commit

---
+++ prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by 全玲玲 on 2017/6/15.
+//  Copyright © 2017年 全玲玲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
