a983a375810a881e408369b8380c8eb0d2fed3fa
全玲玲 <aleen@quanlinglingdeMacBook-Pro.local>
2017-06-18 15:18:12 +0800

    2017-06-19 commit

--- prog1/main.m
+++ prog1/main.m
@@ -8,10 +8,15 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int main(int argc,const char * argv[])
+{
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        //insert code here...
+        NSLog(@"hello,world!");
+        
     }
-    return 0;
+    @return @;
 }
+
